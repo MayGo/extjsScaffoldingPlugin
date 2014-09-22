ee473556ab47f300fec84b4c3fb4d238983e4706
Maigo Erit <maigo.eri@gmail.com>
2014-09-22 15:15:55 +0300

    formatting

--- src/templates/scaffolding/application/app/view/login/Login.js
+++ src/templates/scaffolding/application/app/view/login/Login.js
@@ -1,57 +1,56 @@
-Ext
-		.define(
-				'Horizon.view.login.Login',
-				{
-					extend : 'Ext.window.Window',
-					xtype : 'login',
+Ext.define('Horizon.view.login.Login',{
+	extend : 'Ext.window.Window',
+	xtype : 'login',
 
-					requires : [ 'Horizon.view.login.LoginController',
-							'Ext.form.Panel' ],
+	requires : [ 
+	             'Horizon.view.login.LoginController',
+			     'Ext.form.Panel'
+	           ],
 
-					controller : 'login',
-					bodyPadding : 10,
-					title : 'Login Window',
-					closable : false,
-					autoShow : true,
+	controller : 'login',
+	bodyPadding : 10,
+	title : 'Login Window',
+	closable : false,
+	autoShow : true,
 
-					items : {
-						xtype : 'form',
-						reference : 'form',
-						jsonSubmit : true,
-						defaults : {
-							listeners : {
-								specialkey : function(field, event, options) {
-									// Enable submitting with enter in field
-									if (event.getKey() == event.ENTER) {
-										var loginBtn = field.up('form').down(
-												'button#loginBtn');
-										if (loginBtn.disabled == false)
-											loginBtn.fireHandler();
-									}
-								}
-							}
-						},
-						items : [ {
-							xtype : 'textfield',
-							name : 'username',
-							fieldLabel : 'Username',
-							allowBlank : false
-						}, {
-							xtype : 'textfield',
-							name : 'password',
-							inputType : 'password',
-							fieldLabel : 'Password',
-							allowBlank : false
-						}, {
-							xtype : 'displayfield',
-							hideEmptyLabel : false,
-							value : 'Enter any non-blank password'
-						} ],
-						buttons : [ {
-							text : 'Login',
-							itemId : 'loginBtn',
-							formBind : true,
-							handler : 'onLogin'
-						} ]
+	items : {
+		xtype : 'form',
+		reference : 'form',
+		jsonSubmit : true,
+		defaults : {
+			listeners : {
+				specialkey : function(field, event, options) {
+					// Enable submitting with enter in field
+					if (event.getKey() == event.ENTER) {
+						var loginBtn = field.up('form').down(
+								'button#loginBtn');
+						if (loginBtn.disabled == false)
+							loginBtn.fireHandler();
 					}
-				});
+				}
+			}
+		},
+		items : [ {
+			xtype : 'textfield',
+			name : 'username',
+			fieldLabel : 'Username',
+			allowBlank : false
+		}, {
+			xtype : 'textfield',
+			name : 'password',
+			inputType : 'password',
+			fieldLabel : 'Password',
+			allowBlank : false
+		}, {
+			xtype : 'displayfield',
+			hideEmptyLabel : false,
+			value : 'Enter any non-blank password'
+		} ],
+		buttons : [ {
+			text : 'Login',
+			itemId : 'loginBtn',
+			formBind : true,
+			handler : 'onLogin'
+		} ]
+	}
+});
