addfacbaa63b62ee8b09438f5b4d006d1bd07f71
Maigo Erit <maigo.eri@gmail.com>
2014-10-02 10:37:24 +0300

    Add menu grouping. Add random fontawsome glyphs

--- src/templates/scaffolding/application/app/view/menu/TopMenu.js
+++ src/templates/scaffolding/application/app/view/menu/TopMenu.js
@@ -18,31 +18,77 @@ Ext.define('${appName}.view.menu.TopMenu', {
                 text: '<b>${appName}</b>'
             },\
             <%
-            int i = 70
-            for(d in domainClasses){%>
-            	{
-            		xtype: 'splitbutton',
-            		text: '${d.getShortName()}',
-            		value: '${d.getShortName()}',
-            		glyph: $i,
-            		handler: 'onClickMenuItem',
-                    menu: [
-                        { 
-                        	text: 'Search items',
-                        	handler: 'onOpenSearch',
-                        	value: '${d.getShortName()}',
-                        	glyph: 0xf002
-                        },
-                        { 
-                        	text: 'Create new',
-                        	handler: 'onCreateDomainObject',
-                        	value: '${d.getShortName()}',
-                        	glyph: 0xf067 
-                        }
-                    ]
-            	},\
-            <%
-            i++
-            }%>]
+            def charList = new ArrayList(61506..61802)
+            Collections.shuffle(charList)
+            int i = 0
+            if(domainClasses.size() < 8){
+	            for(d in domainClasses){%>
+	            	{
+	            		xtype: 'splitbutton',
+	            		text: '${d.getShortName()}',
+	            		value: '${d.getShortName()}',
+	            		glyph: ${charList[i]},
+	            		handler: 'onClickMenuItem',
+	                    menu: [
+	                        { 
+	                        	text: 'Search items',
+	                        	handler: 'onOpenSearch',
+	                        	value: '${d.getShortName()}',
+	                        	glyph: 0xf002
+	                        },
+	                        { 
+	                        	text: 'Create new',
+	                        	handler: 'onCreateDomainObject',
+	                        	value: '${d.getShortName()}',
+	                        	glyph: 0xf067 
+	                        }
+	                    ]
+	            	},\
+	            <%
+	            i++
+	            }
+            }else{//Group menu items based on package name
+            	Map groupedDomainClasses = domainClasses.groupBy([{it.packageName}])
+            	groupedDomainClasses.each{packageName, domainClasses->
+            	i++
+            	%>
+	            	{
+	            		xtype: 'splitbutton',
+	            		text: '${packageName.tokenize('.').last()}',
+	            		glyph: ${charList[i]},
+	                    menu:[
+	                    <% domainClasses.each{d->
+	                    i++
+	                    %>
+		            		{
+			            		text: '${d.getShortName()}',
+			            		value: '${d.getShortName()}',
+			            		handler: 'onClickMenuItem',
+			            		glyph: ${charList[i]},
+		                    	menu: [
+			                        { 
+			                        	text: 'Search items',
+			                        	handler: 'onOpenSearch',
+			                        	value: '${d.getShortName()}',
+			                        	glyph: 0xf002
+			                        },
+			                        { 
+			                        	text: 'Create new',
+			                        	handler: 'onCreateDomainObject',
+			                        	value: '${d.getShortName()}',
+			                        	glyph: 0xf067 
+			                        }
+			                    ]
+		            		},
+	                    <%
+	                   
+	                    }%>
+	                    ]
+	            	},\
+            	<%
+            	 
+            	}
+            }
+	           %>]
     }]		
 });
